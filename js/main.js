a5ea3295dc2e4ea7d2fd838a14d695f4936cbdee
JustinErenJaeger <soulcali789@gmail.com>
2019-02-21 11:49:00 +0200

    fixed responsiveness

---
+++ js/main.js
@@ -0,0 +1,7 @@
+function sidebar(){
+    document.getElementById("navbar").classList.toggle('active');
+}
+
+function skills_toggle() {
+    document.getElementById("skills").classList.toggle('toggle');
+}
